984a6945bd7e28ebb5a117c9988a18759c2f8e98
Christian Kopač <hello@christiankopac.com>
2025-10-02 04:14:23 +0200

    Additional folders

---
+++ custom.el
@@ -0,0 +1,32 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/home/christian/Sync/org/agenda/inbox.org"
+     "/home/christian/Sync/org/agenda/log.org"
+     "/home/christian/Sync/org/agenda/gtd/areas.org"
+     "/home/christian/Sync/org/agenda/gtd/horizons.org"
+     "/home/christian/Sync/org/agenda/gtd/inbox.org"
+     "/home/christian/Sync/org/agenda/gtd/projects.org"
+     "/home/christian/Sync/org/agenda/gtd/tasks.org"
+     "/home/christian/Sync/org/agenda/gtd/vision.org"
+     "/home/christian/Documents/Sync/org/agenda/journal/2025-10-02.org"))
+ '(package-selected-packages '(copilot everforest-theme))
+ '(package-vc-selected-packages
+   '((copilot :url "https://github.com/copilot-emacs/copilot.el" :branch "main")
+     (everforest-theme :url
+                       "https://github.com/Theory-of-Everything/everforest-emacs.git")))
+ '(warning-suppress-log-types
+   '((native-compiler) (native-compiler) (native-compiler) (native-compiler)
+     (copilot copilot-no-mode-indent))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(jinx-misspelled ((t (:underline t :foreground nil))))
+ '(org-document-info-keyword ((t (:underline nil))))
+ '(org-modern-date-inactive ((t (:inherit org-modern-label :background "gray20" :foreground "gray70" :family "MonoLisa Nerd Font")))))
